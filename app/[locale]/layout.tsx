a5a09bbaa40a337f2f67865a8df044fc2ecdf756
Nikollas Betiol <nikollasdev@gmail.com>
2025-10-27 11:38:37 -0400

    feat(): adding tag manager

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -112,8 +112,27 @@ export default async function LocaleLayout({
         <meta name="google-adsense-account" content="ca-pub-5771662142995562" />
         <link rel="icon" href="/favicon.ico" />
         <StructuredData />
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MRGTQWDZ');`,
+          }}
+        />
       </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MRGTQWDZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5771662142995562"
